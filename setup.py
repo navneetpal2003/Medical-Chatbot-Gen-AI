890979fd53d38945f2274b146716f934deb6ce8a
Nav Neet Pal <137805006+navneetpal2003@users.noreply.github.com>
2025-08-17 06:58:41 +0000

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative ai Project',
+    version='0.0.0',
+    author= 'Nav Neet Pal',
+    author_email='navneetpalmrt2003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
